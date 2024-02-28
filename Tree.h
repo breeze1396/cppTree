e9b9558d51cbf3aae690eb8161d13f605ee4888b
微风中的快乐 <2329484200@qq.com>
2024-02-28 10:46:25 +0800

    【add】增加的树的几个方法

--- Tree.h
+++ Tree.h
@@ -9,11 +9,19 @@
 #include <map>
 #include <set>
 
+using std::string;
+
 template <typename T>
 class Tree
 {
 public:
 	void setTree(std::string tree_str);
+	void getTree() const;
+	void printTree() const;
+private:
+	void setTreeWithPrefixAndMid(string pre_str, mid_str);
+	void setTreeWithSharp(string tree_str);
+
 private:
 	T node_;
 
