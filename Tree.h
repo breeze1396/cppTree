b1f40381d3c75fe76d7cc13b39dc60abce9d9df4
微风中的快乐 <2329484200@qq.com>
2024-02-28 10:40:01 +0800

    【add】增加了todo事项

--- Tree.h
+++ Tree.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <set>
+
+template <typename T>
 class Tree
 {
+public:
+	void setTree(std::string tree_str);
+private:
+	T node_;
+
 };
 
